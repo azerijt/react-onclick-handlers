1031f390f1a3198f0eb8a1e4511e60903f4110ce
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-03-07 14:39:29 +0000

    completed exercises

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,6 @@
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+  const handleClick = () => window.open("https://www.w3schools.com")
+  return <button onClick={handleClick}>Make me do something when I'm clicked...</button>;
 }
 
 export default TestButton;
